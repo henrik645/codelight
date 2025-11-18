6a763c95b51176e79ff527b60102283d5ba02fc5
Henrik Vester <hves@netlight.com>
2025-11-18 09:33:23 +0100

    First day

---
+++ 1/Main.hs
@@ -0,0 +1,18 @@
+import Data.Maybe (mapMaybe)
+
+inputFile :: String
+inputFile = "input.txt"
+
+main :: IO ()
+main = do
+    content <- fmap lines $ readFile inputFile
+    print $ number isPalindrome $ map removeNonEnglish content
+
+number :: (a -> Bool) -> [a] -> Int
+number p = length . filter id . map p
+        
+removeNonEnglish :: String -> String
+removeNonEnglish = filter (`elem` ['a'..'z'])
+
+isPalindrome :: String -> Bool
+isPalindrome str = (reverse str) == str
